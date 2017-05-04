2ae9892631e93db22e3eb9eee688cf0d2cfa7002
teor <teor2345@gmail.com>
2017-05-04 13:33:58 +1000

    Remove unnecessary headers in control.c

--- src/or/control.c
+++ src/or/control.c
@@ -70,17 +70,6 @@
 #include "routerlist.h"
 #include "routerparse.h"
 
-#include "sys/socket.h"
-#include "sys/types.h"
-#include "netinet/in.h"
-#include "netdb.h"
-#include "stdio.h"
-#include "string.h"
-#include "stdlib.h"
-#include "unistd.h"
-#include "errno.h"
-#include "arpa/inet.h"
-
 #ifndef _WIN32
 #include <pwd.h>
 #include <sys/resource.h>
