c11a2d86086126eb39bf5152310403fce85ed6c1
Rob Jansen <jansen@cs.umn.edu>
2018-02-05 18:11:43 -0500

    code to track packets, tor builds now

--- src/or/tmodel.h
+++ src/or/tmodel.h
@@ -12,7 +12,10 @@
 
 typedef enum tmodel_action_e tmodel_action_t;
 enum tmodel_action_e {
-    TMODEL_SENT_TO_CIRC_INITIATOR, TMODEL_RECV_FROM_CIRC_INITIATOR
+    TMODEL_OBS_NONE,
+    TMODEL_OBS_SENT_TO_ORIGIN,
+    TMODEL_OBS_RECV_FROM_ORIGIN,
+    TMODEL_OBS_DONE,
 };
 
 /* An opaque structure representing stream model info. The internals
@@ -20,11 +23,11 @@ enum tmodel_action_e {
  * tmodel class. */
 typedef struct tmodel_stream_s tmodel_stream_t;
 
-tmodel_stream_t* tmodel_stream_new();
-void tmodel_stream_cell_transferred(tmodel_stream_t* tstream, size_t length, tmodel_action_t action);
+tmodel_stream_t* tmodel_stream_new(void);
+void tmodel_stream_cell_transferred(tmodel_stream_t* tstream, size_t length, tmodel_action_t obs);
 void tmodel_stream_free(tmodel_stream_t* tstream);
 
 int tmodel_set_traffic_model(uint32_t len, char *body);
-int tmodel_is_active();
+int tmodel_is_active(void);
 
 #endif /* SRC_OR_TMODEL_H_ */
