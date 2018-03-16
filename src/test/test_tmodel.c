988db8d7e2bc3d1f89ebf3d7ba0ffb58ac7e9b11
Rob Jansen <jansen@cs.umn.edu>
2018-03-16 06:47:51 -0400

    Fix time in tmodel test, add more test cases.

--- src/test/test_tmodel.c
+++ src/test/test_tmodel.c
@@ -15,17 +15,19 @@
 #include "test.h"
 #include "test_helpers.h"
 
+#define PACKET_MODEL_DICT "{\"observation_space\":[\"+\";\"-\";\"F\"];\"emission_probability\":{\"s1\":{\"+\":[0.1;3.8;1.7;0.0];\"-\":[0.9;1.4;0.9;0.0]};\"s0\":{\"+\":[0.8;12.0;0.01;0.0];\"-\":[0.2;5.5;3.0;0.0]};\"End\":{\"F\":[1.0]}};\"state_space\":[\"s0\";\"s1\";\"End\"];\"transition_probability\":{\"s1\":{\"s0\":0.5;\"End\":0.5};\"s0\":{\"s1\":0.25;\"s0\":0.75};\"End\":{}};\"start_probability\":{\"s1\":0.8;\"s0\":0.2}}"
+#define STREAM_MODEL_DICT "{\"observation_space\":[\"$\";\"F\"];\"emission_probability\":{\"s2End\":{\"F\":[1.0]};\"s1Dwell\":{\"$\":[1.0;2980957.987041;1.36;0.0]};\"s0Active\":{\"$\":[1.0;0.0;0.0;0.00015]}};\"state_space\":[\"s0Active\";\"s1Dwell\";\"s2End\"];\"transition_probability\":{\"s2End\":{};\"s1Dwell\":{\"s2End\":0.34;\"s1Dwell\":0.495;\"s0Active\":0.165};\"s0Active\":{\"s2End\":0.34;\"s1Dwell\":0.165;\"s0Active\":0.495}};\"start_probability\":{\"s2End\":0.0;\"s1Dwell\":0.5;\"s0Active\":0.5}}"
 const char* tmodel_str =
-    "TRUE {\"packet_model\":{\"observation_space\":[\"+\";\"-\";\"F\"];\"emission_probability\":{\"s1\":{\"+\":[0.1;3.8;1.7;0.0];\"-\":[0.9;1.4;0.9;0.0]};\"s0\":{\"+\":[0.8;12.0;0.01;0.0];\"-\":[0.2;5.5;3.0;0.0]};\"End\":{\"F\":[1.0]}};\"state_space\":[\"s0\";\"s1\";\"End\"];\"transition_probability\":{\"s1\":{\"s0\":0.5;\"End\":0.5};\"s0\":{\"s1\":0.25;\"s0\":0.75};\"End\":{}};\"start_probability\":{\"s1\":0.8;\"s0\":0.2}};\"stream_model\":{\"observation_space\":[\"$\";\"F\"];\"emission_probability\":{\"s2End\":{\"F\":[1.0]};\"s1Dwell\":{\"$\":[1.0;2980957.987041;1.36;0.0]};\"s0Active\":{\"$\":[1.0;0.0;0.0;0.00015]}};\"state_space\":[\"s0Active\";\"s1Dwell\";\"s2End\"];\"transition_probability\":{\"s2End\":{};\"s1Dwell\":{\"s2End\":0.34;\"s1Dwell\":0.495;\"s0Active\":0.165};\"s0Active\":{\"s2End\":0.34;\"s1Dwell\":0.165;\"s0Active\":0.495}};\"start_probability\":{\"s2End\":0.0;\"s1Dwell\":0.5;\"s0Active\":0.5}}}";
+    "TRUE {\"packet_model\":"PACKET_MODEL_DICT";\"stream_model\":"STREAM_MODEL_DICT"}";
+const char* packet_model_str =
+    "TRUE {\"packet_model\":"PACKET_MODEL_DICT"}";
+const char* stream_model_str =
+    "TRUE {\"stream_model\":"STREAM_MODEL_DICT"}";
 
-// FIXME monotime_coarse_set_mock_time_nsec doesn't seem to be setting a nonzero time
-// the true result should be the following
-//const char* viterbi_str =
-//    "[[\"s1\";\"+\";1000];[\"s0\";\"+\";1000];[\"s0\";\"-\";1000];[\"s1\";\"-\";1000];[\"End\";\"F\";1000]]";
 const char* viterbi_packets_str =
-  "[[\"s1\";\"+\";0];[\"s0\";\"+\";0];[\"s0\";\"-\";0];[\"s1\";\"-\";0];[\"End\";\"F\";0]]";
+    "[[\"s1\";\"+\";1000];[\"s0\";\"+\";1000];[\"s0\";\"-\";1000];[\"s1\";\"-\";1000];[\"End\";\"F\";0]]";
 const char* viterbi_streams_str =
-  "[[\"s0Active\";\"$\";0];[\"s0Active\";\"$\";0];[\"s0Active\";\"$\";0];[\"s2End\";\"F\";0]]";
+    "[[\"s0Active\";\"$\";1000];[\"s0Active\";\"$\";1000];[\"s0Active\";\"$\";1000];[\"s2End\";\"F\";0]]";
 
 static void
 control_event_privcount_viterbi_mock(char* viterbi_result) {
@@ -102,21 +104,45 @@ tor_libevent_get_base_mock(void)
   return mock_event_base;
 }
 
-static void
-test_traffic_model_parse(void *arg) {
-  (void) arg;
+static uint64_t test_traffic_model_common_setup_helper(int num_threads,
+    void* packets_mock, void* streams_mock) {
   or_options_t *options = get_options_mutable();
   options->EnablePrivCount = 1;
-  options->PrivCountNumViterbiWorkers = 0;
+  options->PrivCountNumViterbiWorkers = num_threads;
 
   monotime_enable_test_mocking();
+
+  MOCK(control_event_privcount_viterbi_packets, packets_mock);
+  MOCK(control_event_privcount_viterbi_streams, streams_mock);
+
+  mock_event_init();
+  MOCK(tor_libevent_get_base, tor_libevent_get_base_mock);
+
   uint64_t now_ns = 1389631048 * (uint64_t) 1000000000;
-  monotime_coarse_set_mock_time_nsec(now_ns);
+  monotime_set_mock_time_nsec(now_ns);
+  return now_ns;
+}
 
-  //printf("%s\n", tmodel_str);
+static uint64_t test_traffic_model_common_setup(int num_threads) {
+  return test_traffic_model_common_setup_helper(num_threads,
+      control_event_privcount_viterbi_packets_mock,
+      control_event_privcount_viterbi_streams_mock);
+}
 
-  int result = tmodel_set_traffic_model((uint32_t) strlen(tmodel_str),
-      tmodel_str);
+static void test_traffic_model_common_teardown(void) {
+  mock_event_free_all();
+  UNMOCK(tor_libevent_get_base);
+  UNMOCK(control_event_privcount_viterbi_packets);
+  UNMOCK(control_event_privcount_viterbi_streams);
+  monotime_disable_test_mocking();
+}
+
+static void
+test_traffic_model_parse_helper(const char* model_str, int num_threads) {
+  test_traffic_model_common_setup(num_threads);
+
+  uint32_t model_str_len = (uint32_t) strlen(model_str);
+  int result = tmodel_set_traffic_model(model_str_len, model_str);
   tt_assert(result == 0);
 
   result = tmodel_set_traffic_model((uint32_t) 5, "FALSE");
@@ -126,26 +152,193 @@ test_traffic_model_parse(void *arg) {
   tt_assert(result == 0);
 
 done:
-  monotime_disable_test_mocking();
+  test_traffic_model_common_teardown();
   return;
 }
 
 static void
-test_traffic_model_viterbi_threads(void *arg) {
+test_traffic_model_parse_full(void *arg) {
   (void) arg;
-  or_options_t *options = get_options_mutable();
-  options->EnablePrivCount = 1;
-  options->PrivCountNumViterbiWorkers = 1;
+  test_traffic_model_parse_helper(tmodel_str, 0);
+}
 
-  monotime_enable_test_mocking();
+static void
+test_traffic_model_parse_full_threads(void *arg) {
+  (void) arg;
+  test_traffic_model_parse_helper(tmodel_str, 1);
+}
 
-  MOCK(control_event_privcount_viterbi_packets,
-      control_event_privcount_viterbi_mock);
-  MOCK(control_event_privcount_viterbi_streams,
-      control_event_privcount_viterbi_mock);
+static void
+test_traffic_model_parse_stream_only(void *arg) {
+  (void) arg;
+  test_traffic_model_parse_helper(stream_model_str, 0);
+}
 
-  mock_event_init();
-  MOCK(tor_libevent_get_base, tor_libevent_get_base_mock);
+static void
+test_traffic_model_parse_stream_only_threads(void *arg) {
+  (void) arg;
+  test_traffic_model_parse_helper(stream_model_str, 1);
+}
+
+static void
+test_traffic_model_parse_packet_only(void *arg) {
+  (void) arg;
+  test_traffic_model_parse_helper(packet_model_str, 0);
+}
+
+static void
+test_traffic_model_parse_packet_only_threads(void *arg) {
+  (void) arg;
+  test_traffic_model_parse_helper(packet_model_str, 1);
+}
+
+static void
+test_traffic_model_viterbi_packets_helper(const char* model_str, int num_threads) {
+  uint64_t now_ns = test_traffic_model_common_setup(num_threads);
+
+  uint32_t model_str_len = (uint32_t) strlen(model_str);
+  int result = tmodel_set_traffic_model(model_str_len, model_str);
+  tt_assert(result == 0);
+
+  tmodel_packets_t* test_stream = tmodel_packets_new();
+  tt_assert(test_stream);
+
+  /* now simulate some packets getting transferred */
+  now_ns += 1000 * 1000;
+  monotime_set_mock_time_nsec(now_ns);
+
+  tmodel_packets_observation(test_stream,
+      TMODEL_OBSTYPE_PACKET_RECV_FROM_ORIGIN, 1434);
+
+  now_ns += 1000 * 1000;
+  monotime_set_mock_time_nsec(now_ns);
+
+  tmodel_packets_observation(test_stream,
+      TMODEL_OBSTYPE_PACKET_RECV_FROM_ORIGIN, 1434);
+
+  now_ns += 1000 * 1000;
+  monotime_set_mock_time_nsec(now_ns);
+
+  tmodel_packets_observation(test_stream,
+      TMODEL_OBSTYPE_PACKET_SENT_TO_ORIGIN, 1434);
+
+  now_ns += 1000 * 1000;
+  monotime_set_mock_time_nsec(now_ns);
+
+  tmodel_packets_observation(test_stream,
+      TMODEL_OBSTYPE_PACKET_SENT_TO_ORIGIN, 1434);
+
+  now_ns += 1000 * 1000;
+  monotime_set_mock_time_nsec(now_ns);
+
+  tmodel_packets_observation(test_stream,
+      TMODEL_OBSTYPE_PACKETS_FINISHED, 0);
+
+  /* close the stream, which runs viterbi */
+
+  tmodel_packets_free(test_stream);
+
+  result = tmodel_set_traffic_model((uint32_t) 5, "FALSE");
+  tt_assert(result == 0);
+
+done:
+  test_traffic_model_common_teardown();
+  return;
+}
+
+static void
+test_traffic_model_viterbi_packets_full(void *arg) {
+  (void) arg;
+  test_traffic_model_viterbi_packets_helper(tmodel_str, 0);
+}
+
+static void
+test_traffic_model_viterbi_packets_full_threads(void *arg) {
+  (void) arg;
+  test_traffic_model_viterbi_packets_helper(tmodel_str, 1);
+}
+
+static void
+test_traffic_model_viterbi_packets_packets(void *arg) {
+  (void) arg;
+  test_traffic_model_viterbi_packets_helper(packet_model_str, 0);
+}
+
+static void
+test_traffic_model_viterbi_packets_packets_threads(void *arg) {
+  (void) arg;
+  test_traffic_model_viterbi_packets_helper(packet_model_str, 1);
+}
+
+static void
+test_traffic_model_viterbi_streams_helper(const char* model_str, int num_threads) {
+  uint64_t now_ns = test_traffic_model_common_setup(num_threads);
+
+  int result = tmodel_set_traffic_model((uint32_t) strlen(model_str),
+      model_str);
+  tt_assert(result == 0);
+
+  tmodel_streams_t* test_circuit = tmodel_streams_new();
+  tt_assert(test_circuit);
+
+  tmodel_streams_observation(test_circuit, TMODEL_OBSTYPE_STREAM_NEW);
+
+  now_ns += 1000 * 1000;
+  monotime_set_mock_time_nsec(now_ns);
+
+  tmodel_streams_observation(test_circuit, TMODEL_OBSTYPE_STREAM_NEW);
+
+  now_ns += 1000 * 1000;
+  monotime_set_mock_time_nsec(now_ns);
+
+  tmodel_streams_observation(test_circuit, TMODEL_OBSTYPE_STREAM_NEW);
+
+  now_ns += 1000 * 1000;
+  monotime_set_mock_time_nsec(now_ns);
+
+  tmodel_streams_observation(test_circuit, TMODEL_OBSTYPE_STREAMS_FINISHED);
+
+  tmodel_streams_free(test_circuit);
+
+  result = tmodel_set_traffic_model((uint32_t) 5, "FALSE");
+  tt_assert(result == 0);
+
+done:
+  test_traffic_model_common_teardown();
+  return;
+}
+
+static void
+test_traffic_model_viterbi_streams_full(void *arg) {
+  (void) arg;
+  test_traffic_model_viterbi_streams_helper(tmodel_str, 0);
+}
+
+static void
+test_traffic_model_viterbi_streams_full_threads(void *arg) {
+  (void) arg;
+  test_traffic_model_viterbi_streams_helper(tmodel_str, 1);
+}
+
+static void
+test_traffic_model_viterbi_streams_streams(void *arg) {
+  (void) arg;
+  test_traffic_model_viterbi_streams_helper(stream_model_str, 0);
+}
+
+static void
+test_traffic_model_viterbi_streams_streams_threads(void *arg) {
+  (void) arg;
+  test_traffic_model_viterbi_streams_helper(stream_model_str, 1);
+}
+
+static void
+test_traffic_model_viterbi_heavy_threads(void *arg) {
+  (void) arg;
+
+  void* model_no_check_mock = control_event_privcount_viterbi_mock;
+  test_traffic_model_common_setup_helper(1,
+      model_no_check_mock, model_no_check_mock);
 
   int result = tmodel_set_traffic_model((uint32_t) strlen(tmodel_str),
       tmodel_str);
@@ -155,9 +348,6 @@ test_traffic_model_viterbi_threads(void *arg) {
   tt_assert(test_circuit);
 
   for(int i = 0; i < 1000; i++) {
-    uint64_t now_ns = 1389631048 * (uint64_t) 1000000000;
-    monotime_coarse_set_mock_time_nsec(now_ns);
-
     tmodel_streams_observation(test_circuit, TMODEL_OBSTYPE_STREAM_NEW);
 
     tmodel_packets_t* test_stream = tmodel_packets_new();
@@ -182,126 +372,35 @@ test_traffic_model_viterbi_threads(void *arg) {
   tmodel_streams_observation(test_circuit, TMODEL_OBSTYPE_STREAMS_FINISHED);
   tmodel_streams_free(test_circuit);
 
-  result = tmodel_set_traffic_model((uint32_t) 0, NULL);
+  result = tmodel_set_traffic_model((uint32_t) 5, "FALSE");
   tt_assert(result == 0);
 
 done:
-  mock_event_free_all();
-  UNMOCK(tor_libevent_get_base);
-  UNMOCK(control_event_privcount_viterbi_packets);
-  UNMOCK(control_event_privcount_viterbi_streams);
-  monotime_disable_test_mocking();
-  return;
-}
-
-static void
-test_traffic_model_viterbi_packets(void *arg) {
-  (void) arg;
-  or_options_t *options = get_options_mutable();
-  options->EnablePrivCount = 1;
-  options->PrivCountNumViterbiWorkers = 0;
-
-  uint64_t now_ns = 1389631048 * (uint64_t) 1000000000;
-  monotime_enable_test_mocking();
-  monotime_coarse_set_mock_time_nsec(now_ns);
-
-  MOCK(control_event_privcount_viterbi_packets,
-      control_event_privcount_viterbi_packets_mock);
-
-  int result = tmodel_set_traffic_model((uint32_t) strlen(tmodel_str),
-      tmodel_str);
-  tt_assert(result == 0);
-
-  tmodel_packets_t* test_stream = tmodel_packets_new();
-  tt_assert(test_stream);
-
-  /* now simulate some packets getting transferred */
-  now_ns += 1000 * 1000;
-  monotime_coarse_set_mock_time_nsec(now_ns);
-
-  tmodel_packets_observation(test_stream,
-      TMODEL_OBSTYPE_PACKET_RECV_FROM_ORIGIN, 1434);
-
-  now_ns += 1000 * 1000;
-  monotime_coarse_set_mock_time_nsec(now_ns);
-
-  tmodel_packets_observation(test_stream,
-      TMODEL_OBSTYPE_PACKET_RECV_FROM_ORIGIN, 1434);
-
-  now_ns += 1000 * 1000;
-  monotime_coarse_set_mock_time_nsec(now_ns);
-
-  tmodel_packets_observation(test_stream,
-      TMODEL_OBSTYPE_PACKET_SENT_TO_ORIGIN, 1434);
-
-  now_ns += 1000 * 1000;
-  monotime_coarse_set_mock_time_nsec(now_ns);
-
-  tmodel_packets_observation(test_stream,
-      TMODEL_OBSTYPE_PACKET_SENT_TO_ORIGIN, 1434);
-
-  now_ns += 1000 * 1000;
-  monotime_coarse_set_mock_time_nsec(now_ns);
-
-  tmodel_packets_observation(test_stream,
-      TMODEL_OBSTYPE_PACKETS_FINISHED, 0);
-
-  /* close the stream, which runs viterbi */
-
-  tmodel_packets_free(test_stream);
-
-done:
-  UNMOCK(control_event_privcount_viterbi_packets);
-  monotime_disable_test_mocking();
-  return;
-}
-
-static void
-test_traffic_model_viterbi_streams(void *arg) {
-  (void) arg;
-  or_options_t *options = get_options_mutable();
-  options->EnablePrivCount = 1;
-  options->PrivCountNumViterbiWorkers = 0;
-
-  uint64_t now_ns = 1389631048 * (uint64_t) 1000000000;
-  monotime_enable_test_mocking();
-  monotime_coarse_set_mock_time_nsec(now_ns);
-
-  MOCK(control_event_privcount_viterbi_streams,
-      control_event_privcount_viterbi_streams_mock);
-
-  int result = tmodel_set_traffic_model((uint32_t) strlen(tmodel_str),
-      tmodel_str);
-  tt_assert(result == 0);
-
-  tmodel_streams_t* test_circuit = tmodel_streams_new();
-  tt_assert(test_circuit);
-
-  tmodel_streams_observation(test_circuit, TMODEL_OBSTYPE_STREAM_NEW);
-
-  now_ns += 1000 * 1000;
-  monotime_coarse_set_mock_time_nsec(now_ns);
-
-  tmodel_streams_observation(test_circuit, TMODEL_OBSTYPE_STREAM_NEW);
-
-  now_ns += 1000 * 1000;
-  monotime_coarse_set_mock_time_nsec(now_ns);
-
-  tmodel_streams_observation(test_circuit, TMODEL_OBSTYPE_STREAM_NEW);
-  tmodel_streams_observation(test_circuit, TMODEL_OBSTYPE_STREAMS_FINISHED);
-
-  tmodel_streams_free(test_circuit);
-
-done:
-  UNMOCK(control_event_privcount_viterbi_streams);
-  monotime_disable_test_mocking();
+  test_traffic_model_common_teardown();
   return;
 }
 
 struct testcase_t tmodel_tests[] = {
-  { "parse", test_traffic_model_parse, TT_FORK, NULL, NULL },
-  { "viterbi_packets", test_traffic_model_viterbi_packets, TT_FORK, NULL, NULL },
-  { "viterbi_streams", test_traffic_model_viterbi_streams, TT_FORK, NULL, NULL },
-  { "viterbi_threads", test_traffic_model_viterbi_threads, TT_FORK, NULL, NULL },
+  { "parse_full", test_traffic_model_parse_full, TT_FORK, NULL, NULL },
+  { "parse_streams", test_traffic_model_parse_stream_only, TT_FORK, NULL, NULL },
+  { "parse_packets", test_traffic_model_parse_packet_only, TT_FORK, NULL, NULL },
+
+  { "parse_full_threads", test_traffic_model_parse_full_threads, TT_FORK, NULL, NULL },
+  { "parse_streams_threads", test_traffic_model_parse_stream_only_threads, TT_FORK, NULL, NULL },
+  { "parse_packets_threads", test_traffic_model_parse_packet_only_threads, TT_FORK, NULL, NULL },
+
+  { "viterbi_streams_streams", test_traffic_model_viterbi_streams_streams, TT_FORK, NULL, NULL },
+  { "viterbi_streams_full", test_traffic_model_viterbi_streams_full, TT_FORK, NULL, NULL },
+
+  { "viterbi_streams_streams_threads", test_traffic_model_viterbi_streams_streams_threads, TT_FORK, NULL, NULL },
+  { "viterbi_streams_full_threads", test_traffic_model_viterbi_streams_full_threads, TT_FORK, NULL, NULL },
+
+  { "viterbi_packets_packets", test_traffic_model_viterbi_packets_packets, TT_FORK, NULL, NULL },
+  { "viterbi_packets_full", test_traffic_model_viterbi_packets_full, TT_FORK, NULL, NULL },
+
+  { "viterbi_packets_packets_threads", test_traffic_model_viterbi_packets_packets_threads, TT_FORK, NULL, NULL },
+  { "viterbi_packets_full_threads", test_traffic_model_viterbi_packets_full_threads, TT_FORK, NULL, NULL },
+
+  { "viterbi_heavy_threads", test_traffic_model_viterbi_heavy_threads, TT_FORK, NULL, NULL },
   END_OF_TESTCASES
 };
