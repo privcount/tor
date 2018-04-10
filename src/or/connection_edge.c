10eea1a39b22883ca038123cf419a04bd231273c
Rob Jansen <jansen@cs.umn.edu>
2018-04-10 12:19:15 -0400

    move stream end events to consistent location

--- src/or/connection_edge.c
+++ src/or/connection_edge.c
@@ -441,6 +441,29 @@ connection_edge_end(edge_connection_t *conn, uint8_t reason)
 
   control_event_privcount_stream_ended(conn);
 
+  /* if we collect a packet model, send the "no more packets" end
+   * event so that we process the packets on this stream. */
+  if (conn->privcount_tmodel_packets) {
+    /* send the finished event for completeness */
+    tmodel_packets_observation(
+        conn->privcount_tmodel_packets,
+        TMODEL_OBSTYPE_PACKETS_FINISHED, 0);
+    /* free the model state */
+    tmodel_packets_free(conn->privcount_tmodel_packets);
+    conn->privcount_tmodel_packets = NULL;
+  }
+
+  /* if we collect a stream model, mark the stream observation,
+   * but only if the stream was active (transferred a packet).
+   * the active check is done so we stay consistent with the packet
+   * model, which doesn't count streams with no packets. */
+  if (circ && conn->privcount_stream_active &&
+      circ->privcount_tmodel_streams) {
+    tmodel_streams_observation(
+        circ->privcount_tmodel_streams,
+        TMODEL_OBSTYPE_STREAM, conn->privcount_create_time);
+  }
+
   return 0;
 }
 
